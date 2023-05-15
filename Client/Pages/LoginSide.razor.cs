0f6b139d1f7f765ab85d2148563d370a93a3af12
Furkan <eaamuco@students.eaaa.dk>
2023-05-15 13:54:16 +0200

    Login og register side

---
+++ Client/Pages/LoginSide.razor.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Client.Pages
+{
+
+    public class LoginModel
+    {
+        [Required(ErrorMessage = "Email er påkrævet")]
+        [EmailAddress(ErrorMessage = "Ugyldig email-adresse")]
+        public string email { get; set; }
+
+        [Required(ErrorMessage = "Password er påkrævet")]
+        public string password { get; set; }
+    }
+
+}
---
+++ Client/Pages/RegisterBruger.razor.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Client.Pages
+{
+    public class RegisterModel
+    {
+        [Required(ErrorMessage = "Email er påkrævet")]
+        [EmailAddress(ErrorMessage = "Ugyldig email-adresse")]
+
+        
+       
+        public string fulde_navn { get; set; }
+        public string email { get; set; }
+        public int telefon_nummer { get; set; }
+        public DateTime fødselsdag { get; set; }
+        public string cpr_nummer { get; set; }
+        public string brugernavn { get; set; }
+        public string password { get; set; }
+            
+     
+
+    }
+}
