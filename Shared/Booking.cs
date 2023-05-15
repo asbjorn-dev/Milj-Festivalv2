22bb96f33aca80f9ff17277b54218016afb509a3
jakobhaulund <jakobhaulund@hotmail.com>
2023-05-15 15:10:28 +0200

    hent bookinger
    0o

--- Shared/Booking.cs
+++ Shared/Booking.cs
@@ -6,11 +6,12 @@ using System.Threading.Tasks;
 
 namespace MiljøFestivalv2.Shared
 {
-	public class Booking
+	public class Booking : Vagt
 	{
 		public int booking_id { get; set; }
-		public Vagt vagt { get; set;}
-		public Bruger frivillig { get; set; }
+		public Bruger frivllig_id { get; set; }
+		public string fulde_navn { get; set; }
+		public string telefon_nummer { get; set; }
 		public Bruger koordinator { get; set; }
 		public Boolean er_låst { get; set; }
 	}
