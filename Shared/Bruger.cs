836c1fcff29eaf56204625a91d41b7c39076d882
jakobhaulund <jakobhaulund@hotmail.com>
2023-05-16 18:23:33 +0200

    post virker

--- Shared/Bruger.cs
+++ Shared/Bruger.cs
@@ -12,16 +12,16 @@ namespace MiljøFestivalv2.Shared
         {
         }
         public int bruger_id { get; set; }
-        public string rolle { get; set; }
+        public string rolle { get; set; } = "frivillig";
         public string fulde_navn { get; set; }
         public string email { get; set; }
-        public int telefon_nummer { get; set; }
+        public int telefon_nummer { get; set; } 
         public DateTime fødselsdag { get; set; }
         public string cpr_nummer { get; set; }
         public string brugernavn { get; set; }
         public string password { get; set; }
-        public bool er_aktiv { get; set; }
-        public bool er_blacklistet { get; set; }
+        public bool er_aktiv { get; set; } = true;
+        public bool er_blacklistet { get; set; } = false;
     }
 }
 
