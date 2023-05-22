c605501bb6e11e986e92286b928d4802bbd32b4e
Asbjørn Knudsen <eaaatkn@students.eaaa.dk>
2023-05-22 10:08:18 +0200

    fixede login.cs

--- Shared/Login.cs
+++ Shared/Login.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -7,9 +8,11 @@ using System.Threading.Tasks;
 namespace MiljøFestivalv2.Shared
 {
     public class Login
-                                                     {
-       public string Brugernavn { get; set; }
-       public string Password { get; set; }
+    {
+        [Required(ErrorMessage = "Brugernavn er påkrævet")]
+        public string Brugernavn { get; set; }
+        [Required(ErrorMessage = "Password er påkrævet")]
+        public string Password { get; set; }
        public int bruger_id { get; set; }
         // Rolle har tom værdi fordi når en bruger prøver at login SKAL sql have både brugernavn,
         //password og rolle med. Rollerne bliver assignet/tjekket på klienten
