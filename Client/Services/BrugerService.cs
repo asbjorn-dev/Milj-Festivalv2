3b13332ec2e7760594826635835cd892acf0dfa5
jakobhaulund <jakobhaulund@hotmail.com>
2023-05-23 10:58:40 +0200

    ok

--- Client/Services/BrugerService.cs
+++ Client/Services/BrugerService.cs
@@ -9,8 +9,8 @@ namespace Client.Services
     public class BrugerService : IBrugerService
     {
         private readonly HttpClient HttpClient;
-        private string LocalHost = "https://localhost:7155";
-        private string AzureHost = "https://xn--miljfestivalgruppe2-y7b.azurewebsites.net";
+        private string Host = "https://localhost:7155";
+        //private string Host = "https://xn--miljfestivalgruppe2-y7b.azurewebsites.net";
 
         public BrugerService(HttpClient HttpClient)
         {
@@ -18,40 +18,40 @@ namespace Client.Services
         }
         public Task<Bruger[]> HentAlleFrivillige() 
         {
-            var Resultat = HttpClient.GetFromJsonAsync<Bruger[]>($"{LocalHost}/api/brugere/hentallefrivillige");
+            var Resultat = HttpClient.GetFromJsonAsync<Bruger[]>($"{Host}/api/brugere/hentallefrivillige");
             return Resultat;
         }
 
         public async Task TilføjBruger(Bruger bruger)
         {
-            await HttpClient.PostAsJsonAsync($"{LocalHost}/api/brugere/tilfoejfrivillig", bruger);
+            await HttpClient.PostAsJsonAsync($"{Host}/api/brugere/tilfoejfrivillig", bruger);
         }
 
         public async Task<Login> Login(Login brugerinfo)
         {
-            return await HttpClient.GetFromJsonAsync<Login>($"{LocalHost}/api/brugere/login/{brugerinfo.Brugernavn}/{brugerinfo.Password}");
+            return await HttpClient.GetFromJsonAsync<Login>($"{Host}/api/brugere/login/{brugerinfo.Brugernavn}/{brugerinfo.Password}");
            
         }
 
         public async Task SkiftAktivStatus(int bruger_id)
         {
-            await HttpClient.PutAsync($"{LocalHost}/api/brugere/skiftaktivstatus/{bruger_id}", null);
+            await HttpClient.PutAsync($"{Host}/api/brugere/skiftaktivstatus/{bruger_id}", null);
         }
 
         public async Task SkiftBlacklistStatus(int bruger_id)
         {
-            await HttpClient.PutAsync($"{LocalHost}/api/brugere/skiftblackliststatus/{bruger_id}", null);
+            await HttpClient.PutAsync($"{Host}/api/brugere/skiftblackliststatus/{bruger_id}", null);
         }
 
         public async Task<Bruger> HentBrugerSingle(int bruger_id)
         {
-            return await HttpClient.GetFromJsonAsync<Bruger>($"{LocalHost}/api/brugere/hentbrugersingle/{bruger_id}");
+            return await HttpClient.GetFromJsonAsync<Bruger>($"{Host}/api/brugere/hentbrugersingle/{bruger_id}");
         }
 
 
         public async Task UpdateBruger(Bruger updatedBruger)
         {
-            await HttpClient.PutAsJsonAsync($"{LocalHost}/api/brugere/updatebruger/{updatedBruger.bruger_id}", updatedBruger);
+            await HttpClient.PutAsJsonAsync($"{Host}/api/brugere/updatebruger/{updatedBruger.bruger_id}", updatedBruger);
 
         }
 
